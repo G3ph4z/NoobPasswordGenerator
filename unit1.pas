1b419ded159a6478bfcc3fb1b637d23019ff7608
G3ph4z <info@denes.cloud>
2018-02-16 20:42:14 +0100

    v0.0.1
    Really bad syntax, but working fine.

---
+++ unit1.pas
@@ -0,0 +1,78 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+function RandomString(StringLen:Integer;CB1:TCheckBox;CB2:TCheckBox):String;
+var  str:String;
+begin
+    Randomize;
+    str:='abcdefghijklmnopqrstuvwxyz';
+    IF CB1.Checked AND CB2.Checked then
+           str := Concat('abcdefghijklmnopqrstuvwxyz','ABCDEFGHIJKLMNOPQRSTUVWXYZ',',','.','/','!','@','#','$','%','^','&','*','''','"',';','_','(',')',':','|','[',']')
+    Else
+        begin
+          IF CB1.Checked then
+                 str := Concat('abcdefghijklmnopqrstuvwxyz','ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+          else
+              begin
+                IF CB2.Checked then
+                   str := Concat('abcdefghijklmnopqrstuvwxyz',',','.','/','!','@','#','$','%','^','&','*','''','"',';','_','(',')',':','|','[',']');
+              end
+    end;
+    Result:='';
+    repeat
+        Result:=Result+str[Random(Length(str))+1];
+    until(Length(Result)=StringLen)
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  length:SmallInt;
+begin
+    length := StrToint(Edit2.Text);
+
+    IF (length = 0) OR (length < 0) then
+       begin
+            Edit1.Text := 'Length have to bigger than zero';
+       end
+    else
+        begin
+             Edit1.Text := RandomString(length,CheckBox1,CheckBox2)
+        end;
+end;
+
+end.
+
